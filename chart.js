ee9e8d2f2bca20a81fa5155130fd3a379c04f781
Rosie Tempest <rosietempest@gmail.com>
2021-03-15 18:30:30 -0700

    added styling to circles

--- chart.js
+++ chart.js
@@ -2,7 +2,7 @@ const world = 'world.json';
 const earthquakes = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson';
 
 // width and height of svg
-const h = 600
+const h = 700
 const w = 1000
 const ptop = 50
 
@@ -13,8 +13,19 @@ const svg = d3.select('body')
     .attr('height', h)
 
 // create path
-const projection = d3.geoMercator();
-const path = d3.geoPath().projection(projection)
+const projection = d3.geoMercator()
+    .translate([w / 2, h / 2])
+    .scale([100])
+const path = d3.geoPath()
+    .projection(projection)
+
+// size scale for earthquakes
+const quakeScale = d3.scaleLinear().domain([4.5, 10]).range([2, 20])
+
+// colour scale for earthquakes
+var threshold = d3.scaleThreshold()
+    .domain([5, 6, 7, 8, 9, 10])
+    .range(['#370617', "#9D0208", "#DC2F02", "#E85D04", "#F48C06", '#FFBA08'])
 
 // get data
 d3.queue()
@@ -30,27 +41,31 @@ function makeMap(error, world, earthquakes) {
             .data(world.features)
             .enter()
             .append("path")
+            .attr('id', 'world')
             .attr("d", path)
-            .style('fill', 'white')
+            .style('fill', 'none')
             .style('stroke', 'black')
-
-        d3.json('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson', function (error, earthquakes) {
-            if (error) return console.error(error);
-            svg.selectAll("circle")
-                .data(earthquakes.features)
-                .enter()
-                .append("circle")
-                .attr("cx", function (d) {
-                    return projection(d.geometry.coordinates)[0];
-                })
-                .attr("cy", function (d) {
-                    return projection(d.geometry.coordinates)[1];
-                })
-                .attr("r", 5)
-                .style('fill', 'red')
-        });
-
     });
+
+    d3.json('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson', function (error, earthquakes) {
+        if (error) return console.error(error);
+        console.log(earthquakes.features)
+        svg.selectAll("circle")
+            .data(earthquakes.features)
+            .enter()
+            .append("circle")
+            .attr("cx", function (d) {
+                return projection(d.geometry.coordinates)[0];
+            })
+            .attr("cy", function (d) {
+                return projection(d.geometry.coordinates)[1];
+            })
+            .attr("r", d => quakeScale(d.properties.mag))
+            .style('fill', d => threshold(d.properties.mag))
+            .attr('id', 'earthquakes')
+    });
+
+
 
 
 }
